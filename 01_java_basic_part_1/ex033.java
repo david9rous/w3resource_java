12078cb185549fcf5e55dcb0c053189ae7e72d7b
david9rous <david9rous@gmail.com>
2024-07-15 12:37:27 +0200

    s

---
+++ 01_java_basic_part_1/ex033.java
@@ -0,0 +1,25 @@
+/*33. Write a Java program and compute the sum of an integer's digits.
+Input Data:
+Input an integer: 25
+Expected Output
+
+The sum of the digits is: 7*/
+
+
+import java.util.Scanner;
+
+
+public class ex033 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String numString = in.nextLine();
+        int sumOfDigits = 0;
+        for (int i = 0; i < numString.length(); i++) {
+            char numString2 = numString.charAt(i);
+            int num = Integer.parseInt(String.valueOf(numString2));
+            sumOfDigits += num;
+        }
+        System.out.println(sumOfDigits);
+        in.close();
+    }
+}
---
+++ 01_java_basic_part_1/ex035.java
@@ -0,0 +1,27 @@
+/*35. Write a Java program to compute the area of a polygon.
+Area of a polygon = (n*s^2)/(4*tan(π/n))
+where n is n-sided polygon and s is the length of a side
+Input Data:
+Input the number of sides on the polygon: 7
+Input the length of one of the sides: 6
+Expected Output
+
+The area is: 130.82084798405722*/
+
+
+import java.lang.Math;
+import java.util.Scanner;
+
+
+public class ex035 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Pocet stran: ");
+        int numOfSides = in.nextInt();
+        System.out.println("Delka strany: ");
+        int side = in.nextInt();
+        double areaOfPoly = (numOfSides * Math.pow(side, 2)) / (4 * Math.tan(Math.PI / numOfSides));
+        System.out.println(areaOfPoly);
+        in.close();
+    }
+}
