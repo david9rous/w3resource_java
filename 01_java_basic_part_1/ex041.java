63641e65ece1bf56fbed49377ea2f607d049ed18
david9rous <david9rous@gmail.com>
2024-07-16 12:38:20 +0200

    s

---
+++ 01_java_basic_part_1/ex041.java
@@ -0,0 +1,16 @@
+/*41. Write a Java program to print the ASCII value of a given character.
+Expected Output
+
+The ASCII value of Z is :90*/
+
+
+import java.util.Scanner;
+
+
+public class ex041 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		char inputCharacter = in.nextLine();
+		System.out.println();
+	}
+}
